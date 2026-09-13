db8ca40c0365712ae9b83dad71fb8a63c81d7a4d
Odin Arvhage <odinarv@stud.ntnu.no>
2026-09-13 11:29:14 +0200

    Added task 1.2.3 to correlation_analysis.py

--- correlation_analysis.py
+++ correlation_analysis.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("train.csv")
 target = "critical_temp"
-
 def task_1():
     correlation_matrix = df.corr(numeric_only=True)
 
@@ -32,4 +31,27 @@ def task_1():
     plt.title(f"Top 15 correlation coefficients for {target}")
     plt.show()
 
+def task_3():
+    corr_matrix = df.corr(numeric_only=True)
+    high_corr_pairs = []
+    for i in range(len(corr_matrix.columns)):
+        for j in range(i + 1, len(corr_matrix.columns)):
+            corr = corr_matrix.iloc[i, j]
+
+            if abs(corr) > 0.9:
+                high_corr_pairs.append(
+                    (
+                        corr_matrix.columns[i],
+                        corr_matrix.columns[j],
+                        corr
+                    )
+                )
+
+    high_corr_pairs.sort(key=lambda x: abs(x[2]), reverse=True) #sort by absolute correlation
+    print("Highly correlated feature pairs (|r| > 0.9):\n")
+    for feature1, feature2, corr in high_corr_pairs[:5]:
+        print(f"{feature1} <-> {feature2}: {corr:.3f}")
+
+
 #task_1()
+#task_3()
